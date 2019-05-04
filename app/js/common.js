27a4d7dd577fcf8c8dbfe5012bdfd79b62516b26
Aleksey <pasyukov.aleksey@qubeek.io>
2019-05-04 21:28:29 +0300

    0.02
    main page

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,35 @@
 $(function() {
 
-	// Custom JS
+	// header shadow on scroll
+	$(document).scroll(function () {
+		$('header').addClass('shadow');
+		if ( $(document).scrollTop() == 0 ) {
+			$('header').removeClass('shadow');
+		}
+	});
+	// end
+	// owl carousel
+
+	$(document).ready(function(){
+		// main-projects
+		$('.owl-carousel-main-projects').owlCarousel({
+			loop: true,
+			margin: 30,
+			nav: true,
+			navContainer: '.owl-nav-main-projects',
+			navText: [ '', '' ],
+			startPosition: 7
+		});
+		// main-partners
+		$('.owl-carousel-main-partners').owlCarousel({
+			loop: true,
+			margin: 30,
+			nav: true,
+			navContainer: '.owl-nav-main-partners',
+			navText: [ '', '' ],
+			startPosition: 1
+		});
+	});
+	//end
 
 });
